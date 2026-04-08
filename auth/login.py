5d17a218c7807e3d9fbc4bffca7ac21bd3c81b40
Vinamra N Gaonkar <vinamragaonkar77@gmail.com>
2026-04-08 16:48:49 +0530

    additional feature

--- auth/login.py
+++ auth/login.py
@@ -1,2 +1,5 @@
-def authenticate():
-    return "[AUTH] Auth OK"
+def authenticate(username, password):
+    if username == "admin" and password == "1234":
+        return "[AUTH] Auth OK"
+    else:
+        return "[AUTH] Auth Failed"
