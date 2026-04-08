fd2466bbf0258d78e87766b28101a0a6bfbbb4d5
vaishnaviganesh25 <vaishnaviganesh251@gmail.com>
2026-04-08 15:25:35 +0530

    fixed utils.py file with suitable logic instead of just returning true

--- telemetry/utils.py
+++ telemetry/utils.py
@@ -1,2 +1,6 @@
-def validate_signal():
-    return True
+def validate_signal(signal):
+    if signal is None:
+        return False
+    if not isinstance(signal, (str, dict, list)):
+        return False
+    return len(str(signal)) > 0
